0645156b4956410f7fb02994c56cd95f5a15fdf6
Benjamin Palmroth <benjipalmroth@gmail.com>
2025-12-11 13:49:34 +0100

    fix input map

--- src/input.ts
+++ src/input.ts
@@ -1,3 +1,5 @@
+
+
 const startInput = document.getElementById("input-start")as HTMLInputElement
 const endInput = document.getElementById("input-end") as HTMLInputElement;
 const btn = document.getElementById("btn-input") as HTMLButtonElement
@@ -16,3 +18,4 @@ btn.addEventListener("click", () => {
   console.log("Start:", startValue);
   console.log("End:", endValue);
 });
+
