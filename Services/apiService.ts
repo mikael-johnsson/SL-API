ef117034374c381752b533a6b165113d95a0438c
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-12-11 10:20:09 +0100

    exempel

--- Services/apiService.ts
+++ Services/apiService.ts
@@ -1,1 +1,11 @@
-.
+export const get = async <T>(url: string): Promise<T> => {
+    const response = await fetch(url);
+  
+    if (!response.ok) {
+      throw new Error(`Kunde inte hämta data från API. Status: ${response.status}`);
+    }
+  
+    const data: T = await response.json();
+    return data;
+  };
+  
---
+++ Services/slApi.ts
@@ -0,0 +1,14 @@
+import type { SLResponse } from "../types/slTypes";
+import { get } from "./apiService";
+
+const BASE_URL = "https://journeyplanner.integration.sl.se/v2/trips";
+
+const buildUrl = (startId: string, destinationId: string, trips: 1 | 2 | 3 = 3) => {
+  return `${BASE_URL}?type_origin=any&type_destination=any&name_origin=${startId}&name_destination=${destinationId}&calc_number_of_trips=${trips}&language=sv`;
+};
+
+export const getTrips = async (startId: string, destinationId: string) => {
+  const url = buildUrl(startId, destinationId);
+  const response = await get<SLResponse>(url);
+  return response.journeys;
+};
