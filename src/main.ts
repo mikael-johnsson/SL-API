8f0bac2e308152f7a4ec9755c751c516c145996c
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-12-11 14:00:34 +0100

    ändringar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import { getStops } from "./Services/stopFinderApi";
-import "./style.css";
-import "./input";
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
-// här kan man testa Stop Finder API-anropet
-const stops = await getStops("slussen");
-console.log(stops);
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
