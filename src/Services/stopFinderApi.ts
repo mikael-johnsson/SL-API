e06abf548a1512fa5ce7a986fa1d6ab0238304f6
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-12-11 13:37:07 +0100

    fix typos

--- src/Services/stopFinderApi.ts
+++ src/Services/stopFinderApi.ts
@@ -4,14 +4,13 @@ import { get } from "./apiService";
 const STOP_BASE_URL = "https://journeyplanner.integration.sl.se/v2/stop-finder";
 
 const buildStopUrl = (searchText: string) => {
-  return `${STOP_BASE_URL}?name_sf=${encodeURIComponent(searchText)}&type_sf=any&any_obj_filter_sf=46`;
+  return `${STOP_BASE_URL}?name_sf=${encodeURIComponent(
+    searchText
+  )}&type_sf=any&any_obj_filter_sf=46`;
 };
-
-https://journeyplanner.integration.sl.se/v2/stop-finder?name_sf=odenplan&any_obj_filter_sf=2&type_sf=any
 
 export const getStops = async (searchText: string) => {
   const url = buildStopUrl(searchText);
   const data = await get<StopFinderResponse>(url);
   return data.locations;
 };
-
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import { getStops } from "./Services/stopFinderApi";
 import "./style.css";
+import "./input";
 
-
+// här kan man testa Stop Finder API-anropet
+const stops = await getStops("slussen");
+console.log(stops);
