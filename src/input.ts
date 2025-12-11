4f22693658426fecd771090cf47c198f64e001d3
Benjamin Palmroth <benjipalmroth@gmail.com>
2025-12-11 13:20:21 +0100

    made ts for btn

--- src/input.ts
+++ src/input.ts
@@ -1,3 +1,18 @@
 const startInput = document.getElementById("input-start")as HTMLInputElement
 const endInput = document.getElementById("input-end") as HTMLInputElement;
 const btn = document.getElementById("btn-input") as HTMLButtonElement
+
+
+btn.addEventListener("click", () => {
+  if (!startInput.value.trim() || !endInput.value.trim()) {
+    alert("Fyll i fälten!");
+    return;
+  }
+
+  const startValue = startInput.value;
+  const endValue = endInput.value;
+
+
+  console.log("Start:", startValue);
+  console.log("End:", endValue);
+});
