a59be93d01af2bf660c3d8bc3c1cc016cdf54f41
Benjamin Palmroth <benjipalmroth@gmail.com>
2025-12-11 11:18:08 +0100

    started with ts

---
+++ src/input.ts
@@ -0,0 +1,3 @@
+const startInput = document.getElementById("input-start")as HTMLInputElement
+const endInput = document.getElementById("input-end") as HTMLInputElement;
+const btn = document.getElementById("btn-input") as HTMLButtonElement
