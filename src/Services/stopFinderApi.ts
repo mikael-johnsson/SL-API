34106cc58f2e9e73bf238ba851b7ead5b6a3a611
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-12-11 13:29:14 +0100

    url sl

--- src/Services/stopFinderApi.ts
+++ src/Services/stopFinderApi.ts
@@ -4,8 +4,10 @@ import { get } from "./apiService";
 const STOP_BASE_URL = "https://journeyplanner.integration.sl.se/v2/stop-finder";
 
 const buildStopUrl = (searchText: string) => {
-  return `${STOP_BASE_URL}?input=${encodeURIComponent(searchText)}&type_origin=any&type_destination=any&language=sv`;
+  return `${STOP_BASE_URL}?name_sf=${encodeURIComponent(searchText)}&type_sf=any&any_obj_filter_sf=46`;
 };
+
+https://journeyplanner.integration.sl.se/v2/stop-finder?name_sf=odenplan&any_obj_filter_sf=2&type_sf=any
 
 export const getStops = async (searchText: string) => {
   const url = buildStopUrl(searchText);
