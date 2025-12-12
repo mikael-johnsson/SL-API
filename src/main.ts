41150c8b04d809e9b256ade31549aa9206cb1529
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-12-12 16:51:24 +0100

    create checkbox to let user choose to use geoLoc or not

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { getStops } from "./Services/stopFinderApi";
 import "./style.css";
 import { getGeoposition } from "./utils/findGeolocation";
+import "./utils/findGeolocation";
 
 const stops = await getStops("slussen");
 console.log("Här är stoppen man får om man söker på slussen: ", stops);
 
+// allt det här bör refaktoreras till en annan fil
 const coords = await getGeoposition();
-console.log(coords);
+if (coords) {
+  console.log("här är användarens geoLocation:", coords);
+}
+
+//hitta checkboxen
+const geoPosCheckbox = document.getElementById(
+  "geopos-checkbox"
+) as HTMLInputElement;
+
+//hitta start input elementet
+const startInput = document.getElementById("input-start") as HTMLInputElement;
+
+//lägg till eventlistener som disablear startinput
+//om användaren vill använda sin platsinfo
+geoPosCheckbox.addEventListener("change", async () => {
+  if (geoPosCheckbox.checked) {
+    console.log("User says - use my geoPosition");
+    startInput.disabled = true;
+  } else {
+    console.log("User says - do not use my geopositon");
+    startInput.disabled = false;
+  }
+});
+//
--- src/utils/findGeolocation.ts
+++ src/utils/findGeolocation.ts
@@ -1,10 +1,16 @@
-const findGePostion = async (): Promise<GeolocationPosition> => {
+const findGeoPostion = async (): Promise<GeolocationPosition> => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
 export const getGeoposition = async () => {
-  const pos = await findGePostion();
-  return pos.coords;
+  try {
+    const pos = await findGeoPostion();
+    return pos.coords;
+  } catch (error) {
+    const geoError = error as GeolocationPositionError;
+    console.log(geoError.message);
+    //här bör felmeddelande skapas för user
+  }
 };
