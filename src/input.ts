b3615ae2275ce1126551eb83f768a677aff956fc
Benjamin Palmroth <benjipalmroth@gmail.com>
2025-12-15 09:20:35 +0100

    suggets input

--- src/input.ts
+++ src/input.ts
@@ -1,11 +1,12 @@
 
+import { getStops } from "./Services/stopFinderApi";
 
 const startInput = document.getElementById("input-start")as HTMLInputElement
 const endInput = document.getElementById("input-end") as HTMLInputElement;
 const btn = document.getElementById("btn-input") as HTMLButtonElement
 
 
-btn.addEventListener("click", () => {
+btn.addEventListener  ("click", async () => {
   if (!startInput.value.trim() || !endInput.value.trim()) {
     alert("Fyll i fälten!");
     return;
@@ -14,8 +15,15 @@ btn.addEventListener("click", () => {
   const startValue = startInput.value;
   const endValue = endInput.value;
 
+  const startStops= await getStops (startValue);
+  const endStops = await getStops( endValue);
 
   console.log("Start:", startValue);
   console.log("End:", endValue);
+
+  console.log("start-station",startStops);
+   console.log("End-station",endStops);
+
+
 });
 
