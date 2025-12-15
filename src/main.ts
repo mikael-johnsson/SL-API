4a50eb8360173b7ca86619d0ea7ddbba6e1c5fe4
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-12-15 10:59:05 +0100

    refactor geolocation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { getStops } from "./Services/stopFinderApi";
 import "./style.css";
-import { getGeoposition } from "./utils/findGeolocation";
-import "./utils/findGeolocation";
+import { getGeoLocation } from "./utils/findGeolocation";
 import { handleGeolocation } from "./utils/handleGeolocation";
 
 const stops = await getStops("slussen");
 console.log("Här är stoppen man får om man söker på slussen: ", stops);
 
-// allt det här bör refaktoreras till en annan fil
-const coords = await getGeoposition();
-if (coords) {
-  console.log("här är användarens geoLocation:", coords);
-}
-
+getGeoLocation();
 handleGeolocation();
--- src/utils/findGeolocation.ts
+++ src/utils/findGeolocation.ts
@@ -1,16 +1,5 @@
-const findGeoPostion = async (): Promise<GeolocationPosition> => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+export const getGeoLocation = () => {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    console.log("this is users position: ", pos.coords);
   });
-};
-
-export const getGeoposition = async () => {
-  try {
-    const pos = await findGeoPostion();
-    return pos.coords;
-  } catch (error) {
-    const geoError = error as GeolocationPositionError;
-    console.log(geoError.message);
-    //här bör felmeddelande skapas för user
-  }
 };
