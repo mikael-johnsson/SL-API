48a921145aae3ece14d0aa5697a60007fbc208fb
Erika Hörling <erikahorling@MacBook-Pro-som-tillhor-Erika-2.local>
2025-12-15 10:40:50 +0100

    try fixing gettripswith coord

--- src/Services/journeyPlannerApi.ts
+++ src/Services/journeyPlannerApi.ts
@@ -8,15 +8,33 @@ const buildUrl = (startId: string, destinationId: string, trips: 1 | 2 | 3 = 3) 
   return `${BASE_URL}?type_origin=any&type_destination=any&name_origin=${startId}&name_destination=${destinationId}&calc_number_of_trips=${trips}&language=sv`;
 };
 
+const buildUrlWithCoords = (startCoords: string, destinationId: string, trips: 1 | 2 | 3 = 1) => {
+  return `${BASE_URL}?type_origin=coords&type_destination=any&name_origin=${startCoords}&name_destination=${destinationId}&calc_number_of_trips=${trips}&language=sv`;
+};
+
 export const getTrips = async (startId: string, destinationId: string) => {
   const url = buildUrl(startId, destinationId);
   const data = await get<JourneyResponse>(url);
   return data.journeys;
 };
 
-export const getUserCoordinates = async () => {
-  const coords = await getGeoposition();
-  return coords;
+export const getTripsWithCoords = async (destinationId: string) => {
+  console.log("inne i funktion")
+
+  const coords = await getGeoposition()
+  if(coords){
+    let coordsString : string = "";
+    coordsString += coords?.latitude.toString();
+    coordsString += ":" + coords?.longitude.toString();
+    coordsString += ":WGS84";
+    console.log ("här är coords string", coordsString);
+  } else {
+    console.log("nåt blev fel")
+  }
 };
+
+
+
+
 
 
