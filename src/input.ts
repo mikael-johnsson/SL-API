821a78604a8313539109cfed3218367e4445a52c
Benjamin Palmroth <benjipalmroth@gmail.com>
2025-12-15 11:01:46 +0100

    add user flow

--- src/input.ts
+++ src/input.ts
@@ -1,11 +1,21 @@
 
 
 import { getStops } from "./Services/stopFinderApi";
+
 export const handleSubmit = () => {
 const startInput = document.getElementById("input-start") as HTMLInputElement;
 const endInput = document.getElementById("input-end") as HTMLInputElement;
 const btn = document.getElementById("btn-input") as HTMLButtonElement;
+ const geoPosCheckbox = document.getElementById( "geopos-checkbox" ) as HTMLInputElement;
 
+  geoPosCheckbox.addEventListener("change", () => {
+    if (geoPosCheckbox.checked) {
+      console.log("User says - use my geoPosition");
+      startInput.disabled = true;
+    } else {
+      ;
+    }
+  });
 
   btn.addEventListener("click",async ()=> {
   if (!startInput.value.trim() || !endInput.value.trim()) {
